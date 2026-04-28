63b3f420991596bc6231e7a282592420af8d285e
inv-ashwin <ashwin.sanalkumar@innovaturelabs.com>
2026-04-28 08:57:59 +0530

    fix: prevent self-collision in folder renaming, correct delete response message, and increase max upload size to 100MB

--- api/views.py
+++ api/views.py
@@ -327,10 +327,10 @@ class FolderUpdateView(APIView):
 
 class FolderDeleteView(APIView):
     def delete(self,request,folder_id):
-        soft_delete_file = FolderService.delete_folder(request.user, folder_id)
+        FolderService.delete_folder(request.user, folder_id)
         return Response({
             "status": "success",
-            "message": "File Restored Sucessfully"
+            "message": "Folder deleted successfully"
         }, status=status.HTTP_200_OK)
 
          
