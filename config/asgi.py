1a6909ab8a2cae5462056707637852e3f29c2027
AshwinSanalkumar <ashwinsanalkumar258@gmail.com>
2026-05-31 18:31:27 +0530

    asgi fix

--- config/asgi.py
+++ config/asgi.py
@@ -8,13 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from api.channels_middleware import CookieJWTAuthMiddleware
-import api.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from api.channels_middleware import CookieJWTAuthMiddleware
+
+import api.routing
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": CookieJWTAuthMiddleware(
