212fc86bc2bc9f36b7a2f6b53198d5402dc59f62
AshwinSanalkumar <ashwinsanalkumar258@gmail.com>
2026-06-02 23:24:17 +0530

    fix : configuration

--- api/urls.py
+++ api/urls.py
@@ -90,7 +90,7 @@ urlpatterns = [
 
 
     path("test-mail/", TestMailView.as_view(), name="test-mail"),
-    path("smtp-test/", SMTPTestView.as_view(), name="test-mail"),
+    path("smtp-test/", SMTPTestView.as_view(), name="smtp-test"),
     # Workstations
     path('workstations/', WorkstationListView.as_view(), name='workstation-list'),
     path('workstations/<uuid:workstation_id>/', WorkstationDetailView.as_view(), name='workstation-detail'),
